88fde49821a883db98d6973f5832a9ac2e448c24
Sh4dowX404Unknown <158760312+Sh4dowX404Unknown@users.noreply.github.com>
2024-02-08 15:55:24 +0100

    Create extractPorts.sh

---
+++ extractPorts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Colors Code
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+END_COLOR="\e[0m"
+
+if [ "$#" -eq 0 ]; then
+	echo -e "\n${RED}[!] Error! No argument supplied!${END_COLOR}"
+	echo -e "\n${YELLOW}[-] Usage: bash extractPorts file${END_COLOR}"
+	exit 1
+fi
+
+# Extracting information
+ports="$(cat $1 | grep -oP '\d{1,5}/' | awk '{print$1}' FS='/' | xargs | tr ' ' ',')"
+ip_address="$(cat $1 | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u)"
+
+# Show the extracted information
+echo -e "\n${YELLOW}[*] Extracting information...${end_color}"
+echo -e "\n${BLUE}[+] IP Address:${END_COLOR} $ip_address"
+echo -e "\n${BLUE}[+] Ports:${END_COLOR} $ports"
+
+# Copying to clipboard
+echo -e "\n\n${YELLOW}[*] Copying ports to clipboard...${END_COLOR}"
+echo $ports | xclip -r -selection c
+echo -e "\n${GREEN}[+] Ports copied to clipboard!${END_COLOR}"
